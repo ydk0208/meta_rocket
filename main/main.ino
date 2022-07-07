66148fe5b71d3c53e38e21011c2f93c9277e0ac0
ydk0208 <ydk208@naver.com>
2022-07-08 00:50:39 +0900

    servo test analoginput
    Servo works well with port A0

--- main/main.ino
+++ main/main.ino
@@ -1,37 +1,69 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
 #include <Servo.h>
- 
-Servo test_motor;
-Servo Top;
-int val=50;
-int cnt = 0;
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
 void setup() {
-  Serial.begin(9600);
-  test_motor.attach(9);      
-  test_motor.write(50);
-  Top.attach(10);
-  delay(3000);                        
+  myservo.attach(A0);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+
+
+
+/*#include <Servo.h>
+ 
+Servo EDF;
+Servo Top;
+Servo Bottom;
+Servo Left;
+Servo Right;
+
+typedef struct axis{
+  double current;
+  double last;
+}axis;
+
+void rocketInit();
+void readIMU();
+void calcIMU();
+void PID();
+void writeMotors();
+
+void setup() {                     
+  rocketInit();
 }
  
 void loop() {
-  if(Serial.available()){
-    char temp = Serial.read();
-    if(temp=='u'){
-      val += 2;
-    }
-    else if(temp=='d'){
-      val -= 2;
-    }
-    test_motor.write(val);
-  }
-  Serial.print("current value : ");
-  Serial.println(val); 
-  delay(100);
-  if(cnt == 0){
-    Top.write(100);
-    cnt = 1;
-  }
-  else {
-    Top.write(50);
-    cnt = 0;
-  }
+  readIMU();
+  calcIMU();
+  PID();
+  writeMotors();
 }
+
+void rocketInit(){
+  EDF.attach(
+}
+*/
