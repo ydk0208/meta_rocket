9c469c008b68536f68c1ea656133956ddc9c4ac5
ydk0208 <ydk208@naver.com>
2022-07-08 01:44:06 +0900

    Skeleton code
    Just skeleton
    need to add : Gyro, telemetry, servos

--- main/main.ino
+++ main/main.ino
@@ -1,39 +1,4 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
-
-void setup() {
-  myservo.attach(A0);  // attaches the servo on pin 9 to the servo object
-}
-
-void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-}
-
-
-
-
-/*#include <Servo.h>
  
 Servo EDF;
 Servo Top;
@@ -42,28 +7,63 @@ Servo Left;
 Servo Right;
 
 typedef struct axis{
-  double current;
-  double last;
+  double kP;
+  double kI;
+  double kD;
+
+  double target = 0;
+  double current = 0;
+  double last = 0;
+  double I = 0;
+  double D = 0;
+  double output = 0;
+  
 }axis;
 
+axis roll;  //motor top/bottom
+axis pitch; //motor left/right
+
 void rocketInit();
+void setGain();
 void readIMU();
 void calcIMU();
-void PID();
+void PID(axis *sys);
 void writeMotors();
 
 void setup() {                     
   rocketInit();
+  setGain(&pitch);
+  setGain(&roll);
 }
  
 void loop() {
   readIMU();
   calcIMU();
-  PID();
+  PID(&pitch);
+  PID(&roll);
   writeMotors();
+  delay(10);
 }
 
 void rocketInit(){
-  EDF.attach(
+  EDF.attach(A0);
+  Top.attach(A1);
+  Bottom.attach(A2);
+  Left.attach(A3);
+  Right.attach(A4);
+
+  Serial.begin(9600);
 }
-*/
+
+void setGain(axis *sys){
+  sys->kP = 1;
+  sys->kD = 0.1;
+  sys->kI = 0.01;
+}
+void PID(axis *sys){
+  double error = sys->target-sys->current;
+  sys->D = error-sys->last;
+  sys->I += error;
+  sys->output = sys->kP*error + sys->kD*sys->D + sys->kI*sys->I;
+  sys->last = error;
+}
